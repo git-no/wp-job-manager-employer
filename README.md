a3b54f03bc0b5503d01c509051dfd25639cfd2a5
Dr. No <mo+ita-git@depeche.email>
2020-05-31 21:52:06 +0200

    Docker for development, repo restructured

---
+++ README.md
@@ -0,0 +1,8 @@
+# WP Job Manager Employer Plugin
+
+## Installation
+
+Copy the folder plugins/wp-job-manager-employer of this repo into your Wordpress plugin folder.
+
+## Development
+
---
+++ plugins/wp-job-manager-employer/public/partials/wp-job-manager-employer-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://www.swissmation.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Job_Manager_Employer
+ * @subpackage Wp_Job_Manager_Employer/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
